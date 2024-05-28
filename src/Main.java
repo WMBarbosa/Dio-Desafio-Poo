c5115a33fd09e02665736a5a6448b78c3e5365f0
WMBarbosa <wesleymario01@gmail.com>
2024-05-28 11:35:03 -0300

    Mentoria e curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso C#");
+        curso2.setDescricao("curso C#");
+        curso2.setCargaHoraria(10);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria Java");
+        mentoria1.setDescricao("Mentoria java");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(curso2);
+        System.out.println(curso1);
+        System.out.println(mentoria1);
     }
 }
